011d7b4dff80c53e6f0761f2656f0579580c3573
Reggie P <20014093+ensorep@users.noreply.github.com>
2022-07-27 16:52:53 -0400

    basic styling

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,28 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
    <div className={styles.container}>
+    <div className={styles.header}>
     <h1 className={styles.title}>Texhale</h1>
+      <nav className={styles.navbar}>
+        <a className={styles.navItem} href="">about</a>
+        <a className={styles.navItem} href="">services</a>
+        <a className={styles.navItem} href="">portfolio</a>
+        <a className={styles.navItem} href="">blog</a>
+        <a className={styles.navItem} href="">youtube</a>
+        <a className={styles.navItem} href="">contact</a>
+      </nav>
+    </div>
+
+    <div className={styles.content}>
+      <div className={styles.taglineBox}>
+        <h2 className={styles.tagline}>Let's Build Something</h2>
+      </div>
+      <div className={styles.taglineBox2}>
+        <h2 className={styles.tagline2}>To Be Proud Of</h2>
+        <button className={styles.mainCTA}>learn more</button>
+      </div>
+    </div>
+
    </div>
   )
 }
